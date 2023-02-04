2f3b3abea13ae1bd1c8e9c2afc2ff90d2e0dfaf3
“Ravi <patilravi400@gmail.com>
2023-02-05 00:02:17 +0530

    commit

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const blogsController = require("../controllers/blogs-controller");
+const verifyToken = require('./verifyToken');
 
 router.get("/", blogsController.getAllBlogs);
-router.post("/", blogsController.addBlog);
-router.get("/:id", blogsController.getById);
-router.put("/:id", blogsController.updateBlog);
-router.delete("/:id", blogsController.deleteBlog);
+router.post("/",verifyToken, blogsController.addBlog);
+router.get("/:id",verifyToken, blogsController.getById);
+router.put("/:id",verifyToken, blogsController.updateBlog);
+router.delete("/:id",verifyToken, blogsController.deleteBlog);
 
 module.exports = router;
